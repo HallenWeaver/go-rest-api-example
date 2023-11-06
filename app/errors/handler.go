8771ada1fc4d224cc51b418b49de67782d292620
HallenWeaver <alexandre.miquilino@gmail.com>
2023-11-05 23:04:22 -0300

    removed (for now) error handling class

--- app/errors/handler.go
+++
@@ -1,9 +0,0 @@
-package errorhandler
-
-import (
-	"github.com/gin-gonic/gin"
-)
-
-func HandleRequestError(c *gin.Context, statusCode int, err error) {
-	c.JSON(statusCode, gin.H{"error": err.Error()})
-}
